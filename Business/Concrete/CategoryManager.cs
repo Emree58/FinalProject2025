a22faada8da55b4bfde5efbd162b2dd45ccd631d
Emre Arslan <arslann1907@hotmail.com>
2025-04-26 19:47:24 +0200

    Lesson 9 final

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //Is kodlari
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetId(int categoryId)
+        {
+            //Select * from Categories where CategoryId = 3
+            return _categoryDal.Get(c=>c.CategoryId == categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,9 +5,31 @@ using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new(new EfProductDal());
+//Data Transformation Object
+ProductTest();
+//CategoryTest();
 
-foreach (var product in productManager.GetByUnitPrice(40,100))
+
+
+
+
+
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new(new EfProductDal());
+
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + " " + product.CategoryName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
 }
