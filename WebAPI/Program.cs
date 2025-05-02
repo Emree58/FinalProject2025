b59300f5cf1c7834f9bb6f4ae27fbcc40cfee7c7
Emre Arslan <arslann1907@hotmail.com>
2025-05-02 18:27:14 +0200

    Lesson 11 end

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,15 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IProductService, ProductManager>();
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
 
 var app = builder.Build();
 
